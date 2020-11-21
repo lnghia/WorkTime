a205949464e1b379c9794daec7bc0c0366a3584d
Trong Nghia <43461631+n3w-us3r@users.noreply.github.com>
2020-11-22 01:13:52 +0700

    Update settings.py

--- WorkTime/settings.py
+++ WorkTime/settings.py
@@ -144,8 +144,8 @@ MEDIA_URL = '/media/'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'hvpmorhnmsuqeodv')
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER', 'valorantemail20@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
