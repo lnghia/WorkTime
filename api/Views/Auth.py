ce3116b003b7bd04ea4e4ec50f49ba107bed9f9b
n3wus3r <trongnghialedinh@gmail.com>
2020-11-22 22:18:12 +0700

    feature/login and authorization for register_worker

--- api/Views/Auth.py
+++ api/Views/Auth.py
@@ -1,19 +1,26 @@
 from os import name
-from rest_framework.decorators import api_view
-from ..Serializers import WorkerSerializer
+from rest_framework.decorators import api_view, permission_classes
+from ..Serializers import WorkerSerializer, LoginSerializer
 from rest_framework.response import Response
 from django.http import JsonResponse
 from ..Constants.Response import response, make_response
 from ..Models.Worker import Worker
 from ..Util.User_Util import new_user
 from ..Util.Email_Util import send_email
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.decorators import permission_required
+from rest_framework.authtoken.models import Token
+import sys
+
 
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@permission_required('api.add_worker')
 def register_worker(request):
     try:
         request_data = WorkerSerializer(data=request.data)
-
+ 
         if request_data.is_valid():
             name = request_data['name'].value
             email = request_data['email'].value
@@ -23,6 +30,27 @@ def register_worker(request):
             send_email('Account Initiated', email, {'username': username,
                                                     'password': password})
             return JsonResponse(make_response(1,{'id': new_worker.id}, None))
+        return JsonResponse(make_response(0, None, request_data.errors))
+    except:
+        # print(sys.exc_info()[0])
+        return JsonResponse(make_response(0, None, 'server error'))
+
+@api_view(['POST'])
+def login(request):
+    try:
+        request_data = LoginSerializer(data=request.data)
+
+        if request_data.is_valid():
+            try:
+                worker = Worker.objects.get(email=request.data['username'])
+                if worker.verify_password(request.data['password']):
+                    token = worker.generate_token()
+                    return JsonResponse(make_response(1, {'token': str(token)}, None))
+                # print(sys.exc_info()[0])
+                return JsonResponse(make_response(0, None, 'invalid username or password'))
+            except:
+                # print(sys.exc_info()[0])
+                return JsonResponse(make_response(0, None, 'invalid username or password'))
         return JsonResponse(make_response(0, None, request_data.errors))
     except:
         return JsonResponse(make_response(0, None, 'server error'))
